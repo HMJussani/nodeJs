6089bd97258f2b2e0c5fbb541ca08a0d84976b69
HMJussani <hmjussani@gmail.com>
2020-02-10 15:43:49 -0300

    Correções do @dotenorio

--- tutofaz/assets/js/tuto.js
+++ tutofaz/assets/js/tuto.js
@@ -15,18 +15,18 @@ function getDados() {
 
 function criaDivs(interacao) {
     var novaDiv = "";
-    var htmlDiv = 'novaDiv' + interacao;
-    if (interacao === 0) novaDiv = novaDiv + "<h2>" + newTuto.newTitle + "</h2>";
-    novaDiv = novaDiv + "<p>" + newTuto.newSubtitle + "</p>";
-    novaDiv = novaDiv + "<p>" + newTuto.newRoteiro + "</p>";
+    var htmlDiv = "novaDiv" + interacao;
+    if (interacao === 0) novaDiv = `${novaDiv} <h2> ${newTuto.newTitle}</h2>`;
+    novaDiv = `${novaDiv} <p> ${newTuto.newSubtitle} </p>`;
+    novaDiv = `${novaDiv} <p> ${newTuto.newRoteiro} </p>`;
     document.getElementById(htmlDiv).innerHTML = novaDiv;
 
 }
 
 function injetarDiv(interacao) {
     var corpo = document.body;
-    var div = document.createElement('div');
-    div.id = 'novaDiv' + interacao;
+    var div = document.createElement("div");
+    div.id = "novaDiv" + interacao;
     corpo.appendChild(div);
 
 }
